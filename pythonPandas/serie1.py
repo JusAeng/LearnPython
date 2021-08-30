ab6e18bfb4ebd2b6f079c75fa0530c53ff980a59
JusAeng <justo_play@hotmail.com>
2021-08-30 23:05:08 +0700

    BesicSeries

--- pythonPandas/serie1.py
+++ pythonPandas/serie1.py
@@ -1,5 +1,25 @@
 import pandas as pd
-data_ls = [10,20,'jus',14.12,'eiei'] #List
-data_tp = (10,20,'jus',14.12,'eiei') #tuple
-print(pd.Series(data_ls))
-print(pd.Series(data_tp))
+import numpy as np
+
+def besicSeries():
+    data_ls = [10,20,'jus',14.12,'eiei'] #List
+    data_tp = (10,20,'jus',14.12,'eiei') #tuple
+    print(pd.Series(data_ls))
+    print(pd.Series(data_tp))
+
+def withNumpy():
+    ndata = np.array(['jus',10,20,True])
+    # print(ndata,type(ndata))
+    ps=pd.Series(ndata)
+    print(ps,type(ps))
+
+def setIndex():
+    items = ["mango","apple","banana"]
+    idx = [50,20,30]
+    print(pd.Series(items,index=idx))
+def withDic():                                     #access Series
+    fruit={"mango":50,"apple":20,"banana":30}
+    a=pd.Series(fruit)
+    print(a)
+    print(a["apple"],a[0])
+withDic()
